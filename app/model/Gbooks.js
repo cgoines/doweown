6e2f662bf374a9a0050a1cd36ef57462544eabd9
Chip Goines <chip_goines@harvard.edu>
2013-08-13 21:58:53 -0400

    models

--- app/model/Gbooks.js
+++ app/model/Gbooks.js
@@ -2,6 +2,7 @@ Ext.define('doweown.model.Gbooks', {
     extend: 'Ext.data.Model',
     config: {
         fields: [
+		{name: 'title',		type: 'string'},
                 {name: 'thumb',         type: 'string'},
                 {name: 'smallThumb',        type: 'string'}
         ]
