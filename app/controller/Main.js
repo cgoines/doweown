c0d5f404c10d107c8b22e6979d4600057282e6e2
Chip Goines <chip_goines@harvard.edu>
2013-08-15 17:17:53 -0400

    main controller

---
+++ app/controller/Main.js
@@ -0,0 +1,29 @@
+Ext.define('doweown.controller.Main', {
+    extend: 'Ext.app.Controller',
+
+    config: {
+        refs: { 'scanBtn': 'main #scanBtn' },
+    	control: {
+            scanBtn: { tap: 'scanImage' }
+         }
+    },
+ 
+
+    scanImage: function() {
+	var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+        scanner.scan(
+        	function(result) {
+                 	if (result.cancelled)
+                    		Ext.Msg.alert("Scan cancelled.");
+                  	else
+                  		Ext.Msg.alert("we got a barcode: " + result.text);
+                  	//presto search goes here
+                  	//var barcode = result.text;
+                 },
+                 function(error) {
+                 	Ext.Msg.alert("Whoops! Scanning failed: " + error);
+                 }
+    }
+
+
+});
