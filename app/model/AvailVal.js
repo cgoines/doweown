5414d64359b5d56e7b6841748ecef24252339a74
Chip Goines <chip_goines@harvard.edu>
2013-08-22 17:33:15 -0400

    AvailVal model

---
+++ app/model/AvailVal.js
@@ -0,0 +1,9 @@
+Ext.define('doweown.model.AvailVal', {
+    extend: 'Ext.data.Model',
+    config: {
+        fields: [
+                {name: 'text',         type: 'string'},
+                {name: 'url',        type: 'string'},
+        ]
+    }
+});
