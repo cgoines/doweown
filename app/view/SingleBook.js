f28e78aad340857dd2ce5c33e2b5bd2342acb051
Chip Goines <chip_goines@harvard.edu>
2013-08-26 18:55:43 -0400

    view for single hit

---
+++ app/view/SingleBook.js
@@ -0,0 +1,29 @@
+Ext.define('doweown.view.SingleBook', {
+    extend: 'Ext.tab.Panel',
+    xtype: 'singlebook',
+    scrollable: true,
+    styleHtmlContent: true,
+    layout: {
+      type: 'fit',
+      align: 'center'
+     },
+
+    items: [
+       {
+	  docked: 'top',
+ 	  xtype: 'titlebar',
+	  title: 'Search results'
+        },
+        {
+           xtype: 'list',
+	   store: 'Biblio',
+           pinHeaders: false,
+           onItemDisclosure: function( record, btn, index ) {
+                                Ext.Msg.alert('Tap', 'Go to mobile hollis for ' + record.get('title'), Ext.emptyFn);
+            },
+            itemTpl: '<img src="{thumb}"/><strong>{title}</strong> - {author} <br>{description}'
+         }
+
+    ]
+
+});
