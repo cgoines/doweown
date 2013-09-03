62772e0ddabf2212965a24896dbc101f23b70e50
Chip Goines <chip_goines@harvard.edu>
2013-09-03 13:48:24 -0400

    store for collection.online element

--- app/model/Collection.js
+++ app/model/Collection.js
@@ -4,6 +4,7 @@ Ext.define('doweown.model.Collection', {
         belongsTo: 'Branch',
 	hasMany: { model: 'ItemRecord', name: 'items', associationKey: 'itemrecord' },
 	hasOne:  { model: 'HoldTag', name: 'holdtag', associationKey: 'holdtag' },
+	hasOne:  { model: 'Online', name: 'online', associationKey: 'online' },
         fields: [
                 {name: 'collectionname', type: 'string', mapping: 'collectionname'},
                 {name: 'callnumber',     type: 'string', mapping: 'callnumber'}
