ca31cc5ace17d53bdab22f5a8c5d07c1979d82fe
Chip Goines <chip_goines@harvard.edu>
2013-09-04 16:24:46 -0400

    multiple collection associations

--- app/model/Branch.js
+++ app/model/Branch.js
@@ -3,8 +3,10 @@ Ext.define('doweown.model.Branch', {
     config: {
     	hasOne : [
     	  { model: 'doweown.model.Repository', name: 'repository', associationKey: 'repository', mapping: 'repository' },
-          { model: 'doweown.model.Collection', name: 'collection', associationKey: 'collection', mapping: 'collection' },
           { model: 'doweown.model.NoItems',    name: 'noitems',    associationKey: 'noitems',    mapping: 'noitems' }
-    	]
+    	],
+	hasMany : [
+	  { model: 'doweown.model.Collection', name: 'collection', associationKey: 'collection', mapping: 'collection' }
+	]
      }
 });
