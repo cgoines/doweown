352c53322dfd78f0acc6d1671fc84b6d7d498f19
Chip Goines <chip_goines@harvard.edu>
2013-09-04 16:38:52 -0400

    correct mapping

--- app/model/Collection.js
+++ app/model/Collection.js
@@ -2,9 +2,9 @@ Ext.define('doweown.model.Collection', {
     extend: 'Ext.data.Model',
     config: {
         belongsTo: 'Branch',
-	hasMany: { model: 'ItemRecord', name: 'items', associationKey: 'itemrecord' },
-	hasOne:  { model: 'HoldTag', name: 'holdtag', associationKey: 'holdtag' },
-	hasOne:  { model: 'Online', name: 'online', associationKey: 'online' },
+	hasMany: { model: 'doweown.model.ItemRecord', name: 'items', associationKey: 'items' },
+	hasOne:  { model: 'doweown.model.HoldTag', name: 'holdtag', associationKey: 'holdtag'  },
+	hasOne:  { model: 'doweown.model.Online', name: 'online', associationKey: 'online' },
         fields: [
                 {name: 'collectionname', type: 'string', mapping: 'collectionname'},
                 {name: 'callnumber',     type: 'string', mapping: 'callnumber'}
