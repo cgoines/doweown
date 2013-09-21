b3d80e872624026620b79c9f67aee9300bf1d195
Chip Goines <chip_goines@harvard.edu>
2013-09-21 19:56:39 -0400

    added thumbnail

--- app/model/WorldCatBooks.js
+++ app/model/WorldCatBooks.js
@@ -6,8 +6,9 @@ Ext.define('doweown.model.WorldCatBooks', {
                 {name: 'author',        type: 'string', mapping: 'author'},
                 {name: 'publisher',     type: 'string', mapping: 'publisher'},
 		{name: 'date',          type: 'string', mapping: 'date'},
-		{name: 'totalLibCount',	type: 'string', mapping: 'totalLibCount'},
+		{name: 'thumbnail',     type: 'string', mapping: 'thumbnail'},
 		{name: 'ISBN', mapping: 'ISBN'},
+		{name: 'totalLibCount',	type: 'string', mapping: 'totalLibCount'},
 		{name: 'library', mapping: 'library'}
         ]
     }
