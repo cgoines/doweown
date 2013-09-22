dda66659a136b0cac27f8f0101758996ca2c2bab
Chip Goines <chip_goines@harvard.edu>
2013-09-21 21:47:26 -0400

    changed title

--- app/view/SingleBook.js
+++ app/view/SingleBook.js
@@ -1,17 +1,23 @@
 Ext.define('doweown.view.SingleBook', {
     extend: 'Ext.List',
     xtype: 'singlebook',
-
     config: {
-	   title: 'Search Results',
+	   //title: 'Search Results',
+	   title: 'Yes, we do! :)',
            xtype: 'list',
 	   store: 'Biblio',
            pinHeaders: false,
-           onItemDisclosure: function( record, btn, index ) {
-                                //Ext.Msg.alert('Tap', 'Go to mobile hollis for ' + record.get('title'), Ext.emptyFn);
-				this.getParent().push( {xtype: 'availbook'} );
-            },
-            itemTpl: '<img src="{thumb}"/><strong>{title}</strong> - {author} <br>{description}'
+	   itemTpl: '<img src="{thumb}"/><strong>{title}</strong> - {author} <br>{description}',
+	   /*onItemDisclosure: function( record, btn, index ) {
+                                this.getParent().push( {xtype: 'availview'} );
+				this.deselectAll();
+            },*/
+	   listeners: {
+		itemtap: function(list, index, target, record) {
+			this.getParent().push( {xtype: 'availview'} );	
+			this.deselectAll();
+		}
+	    }
      }
 
 
