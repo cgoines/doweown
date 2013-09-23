f58c19d37e2245eaff9eb33c12465bf76422562f
Chip Goines <chip_goines@harvard.edu>
2013-09-23 18:38:29 -0400

    no thumbs url

--- app/config/Config.js
+++ app/config/Config.js
@@ -2,10 +2,11 @@ Ext.define('doweown.config.Config', {
 	singleton: true,
 
 	config: {
+		borrowDirectUrl:  'http://nrs.harvard.edu/urn-3:hul.eother:BD',
 		hollisIsbnUrl:    'http://webservices.lib.harvard.edu/rest/mods/hollis/isbn/',
 		hollisAvailUrl:   'http://webservices.lib.harvard.edu/rest/hollis/avail/',
 		hollisSimilarUrl: 'http://webservices.lib.harvard.edu/rest/hollis/search/dc/?q=title:',
-		googleBooksUrl:   'https://www.googleapis.com/books/v1/volumes?q=isbn:',
+		googleBooksUrl:   'https://www.googleapis.com/books/v1/volumes?q=',
 		noThumbnaillUrl:  'http://books.google.com/googlebooks/images/no_cover_thumb.gif',
 		worldCatUrl:	  'http://www.worldcat.org/webservices/catalog/content/libraries/isbn/',
 		worldCatLibs:     'COO,YUS,PUL,RBN,DRB,ZCU,ZCL,VVT,VYC,YUM,GXR,PAU,PAULM,DRP,PLL',
