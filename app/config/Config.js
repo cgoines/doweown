5d774e4d1374547110f2f03cbba5a101d7edcd74
Chip Goines <chip_goines@harvard.edu>
2013-09-25 18:36:06 -0400

    fixed typos

--- app/config/Config.js
+++ app/config/Config.js
@@ -8,7 +8,7 @@ Ext.define('doweown.config.Config', {
 		hollisAvailUrl:   'http://webservices.lib.harvard.edu/rest/hollis/avail/',
 		hollisSimilarUrl: 'http://webservices.lib.harvard.edu/rest/hollis/search/dc/?q=title:',
 		googleBooksUrl:   'https://www.googleapis.com/books/v1/volumes?q=',
-		noThumbnaillUrl:  'http://books.google.com/googlebooks/images/no_cover_thumb.gif',
+		noThumbnailUrl:   'http://books.google.com/googlebooks/images/no_cover_thumb.gif',
 		worldCatUrl:	  'http://www.worldcat.org/webservices/catalog/content/libraries/isbn/',
 		worldCatLibs:     'COO,YUS,PUL,RBN,DRB,ZCU,ZCL,VVT,VYC,YUM,GXR,PAU,PAULM,DRP,PLL',
 		worldCatDevKey:   ''
