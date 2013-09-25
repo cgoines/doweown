c1b1b845bafc50680f4f0f524efa5a8bbe876328
Chip Goines <chip_goines@harvard.edu>
2013-09-25 15:03:24 -0400

    removed libraries list

--- app/view/WorldCatView.js
+++ app/view/WorldCatView.js
@@ -10,7 +10,7 @@ Ext.define('doweown.view.WorldCatView', {
                                 Ext.Msg.alert( record.get('collection.collectionname'), Ext.emptyFn);
             },*/
             //itemTpl: '<div><b> {repository.name}</b> <tpl for="collection"><br> <i>{collectionname}</i><tpl for="items"><br> <tpl switch="isavail"><tpl case="N" case="n"><font color="red"><strong>Not Available</strong></font><tpl default><font color="green"><strong>Available</strong></font></tpl>  :{stat}<br> Call#: {call}<br> <a href="{req.href}" onclick="window.open(this.href,\'_system\'); return false;">{req.content}</a></br></tpl> <tpl for="holdtag"><br> {availkey} <a href="{availval.url}" onclick="window.open(this.href,\'_system\'); return false;" >{availval.content}</a><br></tpl>  <tpl for="online"> <a href="{url}"  onclick="window.open(this.href,\'_system\'); return false;" >{content}</a><br></tpl> </tpl></div> <hr>'
-	itemTpl: '<div><tpl if="thumbnail"><img src="{thumbnail}" style="float:left; padding: 5px 5px 4px 5px"></tpl> <b>{title}</b><br><i>{author}</i><br> {date}<br> {publisher}<br>{description}<p><i>Other Libraries:</i> <tpl for="library">{institutionName}, </tpl><br><b><a href="{borrowDirectUrl}" onclick="window.open(this.href,\'_system\'); return false;">Get it from Borrow Direct</a></b></div>'
+	itemTpl: '<div><tpl if="thumbnail"><img src="{thumbnail}" style="float:left; padding: 5px 5px 4px 5px"></tpl> <b>{title}</b><br><i>{author}</i><br> {date}<br> {publisher}<br>{description}<p><b><a href="{borrowDirectUrl}" onclick="window.open(this.href,\'_system\'); return false;">Find it on Borrow Direct</a></b></div>'
      }
 
 
