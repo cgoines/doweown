0566e2f0d4bc24bfacb2c5230c6f9d1be66f1410
Chip Goines <chip_goines@harvard.edu>
2013-09-24 23:18:09 -0400

    tue build

--- app/view/AvailBook.js
+++ app/view/AvailBook.js
@@ -5,12 +5,13 @@ Ext.define('doweown.view.AvailBook', {
     config: {
            title: 'Availability',
            xtype: 'list',
-           store: 'AvailStore',
+           store: 'BranchStore',
            pinHeaders: false,
            /*onItemDisclosure: function( record, btn, index ) {
-                                Ext.Msg.alert('Tap', 'Go to mobile hollis for ' + record.get('title'), Ext.emptyFn);
+                                Ext.Msg.alert( record.get('collection.collectionname'), Ext.emptyFn);
             },*/
-            itemTpl: '{repository.name}: {collection.collectionname}'
+            //itemTpl: '{repository.name} <tpl for="collection"><br>{collectionname} {availkey}</tpl>'
+	    itemTpl: '{repository.name} <tpl for="collection"><br>{collectionname}<tpl for="items"><br>is avail: {isavail}<br>{stat}<br>{call}<br>{req.content} {req.href}</br></tpl> <tpl for="holdtag"><br>{availkey} {availval.content}<br>{availval.url}</tpl>  <tpl for="online">{content}: {url}<br></tpl> </tpl><p> {noitems.content}'
      }
 
 
