51e5a1904ae34d7a2368b52b7d2827ee29f7b53b
Chip Goines <chip_goines@harvard.edu>
2013-09-24 23:21:59 -0400

    last update for avail store

--- app/store/AvailStore.js
+++ app/store/AvailStore.js
@@ -1,13 +1,17 @@
 Ext.define('doweown.store.AvailStore', {
         extend: 'Ext.data.Store',
         alias: 'store.AvailStore',
-        model: 'doweown.model.Availability',
-        autoLoad: false,
-	proxy: {
+	config: {
+          model: 'doweown.model.Availability',
+          autoLoad: false,
+	  proxy: {
             type: 'jsonp',
             callbackKey: 'jsonp',
+	    url: '',
 	    reader: {
+		type: 'json',
 	    	rootProperty: 'availability'
 	    }
-        }
+          }
+	}
 });
