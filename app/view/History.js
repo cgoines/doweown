fd65279abb6128dd3d304a2d221afa79cbe1a78d
Chip Goines <chip_goines@harvard.edu>
2013-09-25 22:28:41 -0400

    got rid of sticky select highlight

--- app/view/History.js
+++ app/view/History.js
@@ -5,19 +5,12 @@ Ext.define('doweown.view.History', {
 	   title: 'Search History',
            xtype: 'list',
 	   store: 'HistoryStore',
-           pinHeaders: false,
+	   disableSelection: true,
 	   itemTpl: '<img src="{thumbnail}" style="float:left; padding: 5px 5px 4px 5px"/><b>{title}</b><br><i>{author}</i><br>{date}<br><b>ISBN:</b> {barcode}'
 	   /*onItemDisclosure: function( record, btn, index ) {
                                 this.getParent().push( {xtype: 'availview'} );
 				this.deselectAll();
             },*/
-	   /*
-	   listeners: {
-		itemtap: function(list, index, target, record) {
-			this.getParent().push( {xtype: 'availview'} );	
-			this.deselectAll();
-		}
-	    }*/
      }
 
 
--- app/view/SingleBook.js
+++ app/view/SingleBook.js
@@ -3,21 +3,23 @@ Ext.define('doweown.view.SingleBook', {
     xtype: 'singlebook',
     config: {
 	   //title: 'Search Results',
-	   title: 'Yes, we do! :)',
+	   title: 'Yes, we do!',
            xtype: 'list',
 	   store: 'Biblio',
-           pinHeaders: false,
+	   id: 'singleBookList',
+	   disableSelection: true,
 	   itemTpl: '<img src="{thumb}" style="float:left; padding: 5px 5px 4px 5px"/><b>{title}</b><br><i>{author}</i><br>{date}<br>{publisher}<br>{description}',
 	   /*onItemDisclosure: function( record, btn, index ) {
                                 this.getParent().push( {xtype: 'availview'} );
 				this.deselectAll();
             },*/
-	   listeners: {
+	   /*listeners: {
 		itemtap: function(list, index, target, record) {
-			this.getParent().push( {xtype: 'availview'} );	
-			this.deselectAll();
+			//this.getParent().push( {xtype: 'availview'} );	
+			//this.deselectAll();
+			//this.deselect(index);
 		}
-	    }
+	    }*/
      }
 
 
