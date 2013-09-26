234bc4eca022161c2a0f8dbb443a9f6a0e1568da
Chip Goines <chip_goines@harvard.edu>
2013-09-25 21:24:21 -0400

    wed night commit

--- app/store/PrefsStore.js
+++ app/store/PrefsStore.js
@@ -1,6 +1,7 @@
 Ext.define('doweown.store.PrefsStore', {
         extend: 'Ext.data.Store',
         alias: 'store.PrefsStore',
+	requires: ['Ext.data.proxy.LocalStorage'],
 	config: {
           model: 'doweown.model.Prefs',
           autoLoad: true,
