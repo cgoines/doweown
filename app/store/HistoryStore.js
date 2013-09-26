b161821cb84ce0e700c221e9a9b729d7438207ab
Chip Goines <chip_goines@harvard.edu>
2013-09-25 21:21:20 -0400

    wed night final commit

--- app/store/HistoryStore.js
+++ app/store/HistoryStore.js
@@ -1,12 +1,13 @@
 Ext.define('doweown.store.HistoryStore', {
         extend: 'Ext.data.Store',
         alias: 'store.HistoryStore',
+	requires: ['Ext.data.proxy.LocalStorage'],
 	config: {
           model: 'doweown.model.History',
-          autoLoad: true,
-          proxy: {
+          autoLoad: true
+          /*proxy: {
             type: 'localstorage'
-          }
+          }*/
 
 	}
 });
