74cc45427527feca9fdf26950b16d8ef529e546f
Chip Goines <chip_goines@harvard.edu>
2013-09-25 22:41:35 -0400

    took bold off isbn

--- app/view/History.js
+++ app/view/History.js
@@ -6,7 +6,7 @@ Ext.define('doweown.view.History', {
            xtype: 'list',
 	   store: 'HistoryStore',
 	   disableSelection: true,
-	   itemTpl: '<img src="{thumbnail}" style="float:left; padding: 5px 5px 4px 5px"/><b>{title}</b><br><i>{author}</i><br>{date}<br><b>ISBN:</b> {barcode}'
+	   itemTpl: '<img src="{thumbnail}" style="float:left; padding: 5px 5px 4px 5px"/><b>{title}</b><br><i>{author}</i><br>{publisher}, {date}<br>ISBN: {barcode}'
 	   /*onItemDisclosure: function( record, btn, index ) {
                                 this.getParent().push( {xtype: 'availview'} );
 				this.deselectAll();
