48dac16d190a3a42ec33b18f82c4151aa76788e0
Chip Goines <chip_goines@harvard.edu>
2013-11-01 22:16:35 -0400

    bolded hrefs

--- app/view/AvailView.js
+++ app/view/AvailView.js
@@ -5,11 +5,11 @@ Ext.define('doweown.view.AvailView', {
     config: {
            title: 'Availability',
            store: 'BranchStore',
-	   pinHeaders: true,
+	   pinHeaders: false,
            /*onItemDisclosure: function( record, btn, index ) {
                                 Ext.Msg.alert( record.get('collection.collectionname'), Ext.emptyFn);
             },*/
-            itemTpl: '<div><b> {repository.name}</b> <tpl for="collection"><br> <i>{collectionname}</i><br><tpl if="callnumber">{callnumber}</tpl><tpl for="items"><br> <tpl switch="isavail"><tpl case="N" case="n"><font color="red"><strong>Not Available</strong></font><tpl default><font color="green"><strong>Available</strong></font></tpl>:{stat}<br> Call#: {call}<br> <a href="{req.href}" onclick="window.open(this.href,\'_system\'); return false;">{req.content}</a></br></tpl> <tpl for="holdtag"><br> {availkey} <a href="{availval.url}" onclick="window.open(this.href,\'_system\'); return false;" >{availval.content}</a><br></tpl>  <tpl for="online"> <a href="{url}"  onclick="window.open(this.href,\'_system\'); return false;" >{content}</a><br></tpl> </tpl></div> <hr>'
+            itemTpl: '<div><b> {repository.name}</b> <tpl for="collection"><br> <i>{collectionname}</i><br><tpl if="callnumber">{callnumber}</tpl><tpl for="items"><br> <tpl switch="isavail"><tpl case="N" case="n"><font color="red"><strong>Not Available</strong></font><tpl default><font color="green"><strong>Available</strong></font></tpl>:{stat}<br> Call#: {call}<br> <b><a href="{req.href}" onclick="window.open(this.href,\'_system\'); return false;">{req.content}</a></b></br></tpl> <tpl for="holdtag"><br> {availkey} <b><a href="{availval.url}" onclick="window.open(this.href,\'_system\'); return false;" >{availval.content}</a></b><br></tpl>  <tpl for="online"> <b><a href="{url}"  onclick="window.open(this.href,\'_system\'); return false;" >{content}</a></b><br></tpl> </tpl></div> <hr>'
      }
 
 
