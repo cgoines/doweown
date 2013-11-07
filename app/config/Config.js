5968660ea2f083c104226dfd69c472167b7da3a1
Chip Goines <chip_goines@harvard.edu>
2013-11-07 01:52:29 -0500

    added more email properties

--- app/config/Config.js
+++ app/config/Config.js
@@ -10,7 +10,7 @@ Ext.define('doweown.config.Config', {
 		borrowDirectUrl:  'http://access.harvard.edu/access/servlet/access?__hulaccess_gateway=bdirect&__hulaccess_resource=&query=isbn%3D%22_ISBN_%22',
 		borrowDirectDefaultUrl: 'http://nrs.harvard.edu/urn-3:hul.eother:BD',
 		feedbackUrl:	  'http://feedback.lib.harvard.edu/feedback/feedbackEmail?',
-		feedbackTitle:	  'Patron Acquisition Request',
+		feedbackTitle:	  'Mobile App Feedback',
 		hollisIsbnUrl:    'http://webservices.lib.harvard.edu/rest/mods/hollis/isbn/',
 		hollisAvailUrl:   'http://webservices.lib.harvard.edu/rest/hollis/avail/',
 		hollisSimilarUrl: 'http://webservices.lib.harvard.edu/rest/hollis/search/dc/?q=title:',
