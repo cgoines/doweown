f158feedaa019b1c271a1642d4504b392c8dd4ee
Chip Goines <chip_goines@harvard.edu>
2013-11-07 01:43:59 -0500

    10px padding added to screens

--- app/view/AvailView.js
+++ app/view/AvailView.js
@@ -6,10 +6,11 @@ Ext.define('doweown.view.AvailView', {
            title: 'Availability',
            store: 'BranchStore',
 	   pinHeaders: false,
+	   padding: 10,
            /*onItemDisclosure: function( record, btn, index ) {
                                 Ext.Msg.alert( record.get('collection.collectionname'), Ext.emptyFn);
             },*/
-            itemTpl: '<div style="padding: 5px 5px 4px 5px"><b> {repository.name}</b> <tpl for="collection"><br> <i>{collectionname}</i><br><tpl if="callnumber">{callnumber}</tpl><tpl for="items"><br> <tpl switch="isavail"><tpl case="N" case="n"><font color="red"><b>Not Available</b></font><tpl default><font color="green"><b>Available</b></font></tpl>: {stat}<br>  <tpl if="call">Call#: {call}<br></tpl> <b><a href="{req.href}" onclick="window.open(this.href,\'_system\'); return false;">{req.content}</a></b></br></tpl> <tpl for="holdtag"><br> {availkey} <b><a href="{availval.url}" onclick="window.open(this.href,\'_system\'); return false;" >{availval.content}</a></b><br></tpl>  <tpl for="online"> <b><a href="{url}"  onclick="window.open(this.href,\'_system\'); return false;" >{content}</a></b><br></tpl> </tpl></div> <hr> <div id="renderResetButton1"></div> <p/> ',
+            itemTpl: '<b>{repository.name}</b> <tpl for="collection"><br> <i>{collectionname}</i><br><tpl if="callnumber">{callnumber}</tpl><tpl for="items"><br> <tpl switch="isavail"><tpl case="N" case="n"><font color="red"><b>Not Available</b></font><tpl default><font color="green"><b>Available</b></font></tpl>: {stat}<br>  <tpl if="call">Call#: {call}<br></tpl> <b><a href="{req.href}" onclick="window.open(this.href,\'_system\'); return false;">{req.content}</a></b></br></tpl> <tpl for="holdtag"><br> {availkey} <b><a href="{availval.url}" onclick="window.open(this.href,\'_system\'); return false;" >{availval.content}</a></b><br></tpl>  <tpl for="online"> <b><a href="{url}"  onclick="window.open(this.href,\'_system\'); return false;" >{content}</a></b><br></tpl> </tpl> <hr> <div id="renderResetButton1"></div> <p/>',
 	    listeners: {
 		initialize: function() {
 			var btns = Ext.ComponentQuery.query('#scanreset1');
--- app/view/SingleBook.js
+++ app/view/SingleBook.js
@@ -9,7 +9,7 @@ Ext.define('doweown.view.SingleBook', {
 	   pinHeaders: false,
 	   onItemDisclosure: false,
 	   disableSelection: true,
-	   itemTpl: '<img src="{thumb}" style="float:left; padding: 5px 5px 4px 5px"/><b>{title}</b><br><i>{author}</i><br>{publisher}, {date}<br>ISBN: {isbn}<br>{description}<p><br><i><font color="blue">Tap to see availability</font></i>',
+	   itemTpl: '<div style="padding: 2px 2px 2px 2px"><img src="{thumb}" style="float:left; padding: 5px 5px 4px 5px"/><b>{title}</b><br><i>{author}</i><br>{publisher}, {date}<br>ISBN: {isbn}<br>{description}<p><br><i><font color="blue">Tap to see availability</font></i></div>',
 	   /*onItemDisclosure: function( record, btn, index ) {
                                 this.getParent().push( {xtype: 'availview'} );
 				this.deselectAll();
