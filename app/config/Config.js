eb2d21a49d76cea73c6ca38c7a0e5d22f9d255ed
Chip Goines <chip_goines@harvard.edu>
2013-11-11 21:20:33 -0500

    added worldCatHarvardLibs

--- app/config/Config.js
+++ app/config/Config.js
@@ -16,7 +16,7 @@ Ext.define('doweown.config.Config', {
 		hollisSimilarUrl: 'http://webservices.lib.harvard.edu/rest/hollis/search/dc/?q=title:',
 		googleBooksUrl:   'https://www.googleapis.com/books/v1/volumes?q=',
 		noThumbnailUrl:   'http://books.google.com/googlebooks/images/no_cover_thumb.gif',
-		version:	  '0.98 beta',
+		version:	  '0.99 beta',
 		worldCatUrl:	  'http://www.worldcat.org/webservices/catalog/content/libraries/isbn/',
 		worldCatLibs:     'COO,YUS,PUL,RBN,DRB,ZCU,ZCL,VVT,VYC,YUM,GXR,PAU,PAULM,DRP,PLL',
 		worldCatHarvardLibs: 'HMM,HRA,HTV,BOH,HBS,CLS,HCH,FLL,BHA,HMG,HMZ,HFL,HLS,HHG,KSG,HVL,HMU,HMS,SLR,TOZ,HMY,SYS',
