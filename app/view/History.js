0a6d5fa6c7c1015d960cda9dce02d9e36d109f81
Chip Goines <chip_goines@harvard.edu>
2013-11-12 02:11:18 -0500

    now shows owned status

--- app/view/History.js
+++ app/view/History.js
@@ -8,7 +8,10 @@ Ext.define('doweown.view.History', {
 	   itemId: 'history',
 	   store: 'HistoryStore',
 	   disableSelection: true,
-	   itemTpl: '<img src="{thumbnail}" style="float:left; padding: 5px 5px 4px 5px"/><b>{title}</b><br><i>{author}</i><br>{publisher}, {date}<br>ISBN: {barcode}'
+	   itemTpl: '<img src="{thumbnail}" style="float:left; padding: 5px 5px 4px 5px"/>'+
+		'<b>{title}</b><br><i>{author}</i><br>{publisher}, {date}<br>ISBN: {barcode}<br>'+
+		'<tpl if "owned == true"><b><font color="green">View Hollis record</font></b></tpl>'+
+		'<tpl if "owned == false"><b><font color="red">Not in library collections</font></b></tpl>'
 	   /*onItemDisclosure: function( record, btn, index ) {
                                 this.getParent().push( {xtype: 'availview'} );
 				this.deselectAll();
