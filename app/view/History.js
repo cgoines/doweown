969f5fcb3ed0e06f3b23d7b285fac0cbbe33b189
Chip Goines <chip_goines@harvard.edu>
2013-12-16 20:54:54 -0500

    added 'clear history' button'

--- app/view/History.js
+++ app/view/History.js
@@ -11,11 +11,18 @@ Ext.define('doweown.view.History', {
 	   itemTpl: '<img src="{thumbnail}" style="float:left; padding: 5px 5px 4px 5px"/>'+
 		'<b>{title}</b><br><i>{author}</i><br>{publisher}, {date}<br>ISBN: {barcode}<br>'+
 		'<tpl if="owned==true"><b><font color="green">View Hollis record</font></b></tpl> '+
-		'<tpl if="owned==false"><b><font color="red">Not in library collections</font></b></tpl>'
+		'<tpl if="owned==false"><b><font color="red">Not in library collections</font></b></tpl>',
 	   /*onItemDisclosure: function( record, btn, index ) {
                                 this.getParent().push( {xtype: 'availview'} );
 				this.deselectAll();
             },*/
+	    listeners: {
+                initialize: function() {
+                        var clearButton = new Ext.Button({text: 'Clear History', itemId: 'clearhistory'});
+                        this.add(clearButton);
+                }
+            }
+
      }
 
 
